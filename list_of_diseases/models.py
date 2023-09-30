b1a02c9853a8ea742fc11ad45349959154d83c71
dasha124 <dashakonekb@gmail.com>
2023-09-30 17:48:19 +0000

    recreation

--- list_of_diseases/models.py
+++ list_of_diseases/models.py
@@ -12,7 +12,7 @@ class Sphere(models.Model):
         return self.sphere_name
 
     class Meta:
-        managed = False
+        managed = True
         verbose_name_plural = 'Сфера применения'
 
 
@@ -21,10 +21,8 @@ class Disease(models.Model):
     disease_name = models.CharField(max_length=150)
     general_info = models.CharField(max_length=255)
     simptoms = models.CharField(max_length=255)
-    sphere_id = models.OneToOneField(Sphere, on_delete=models.CASCADE)
-    image = models.TextField(default='')
-
-    
+    sphere_id = models.ForeignKey(Sphere, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='list_of_diseases/static/images')
 
     STATUSES = [
         ('a', 'active'),
@@ -36,48 +34,8 @@ class Disease(models.Model):
         return self.disease_name
 
     class Meta:
+        managed = True
         verbose_name_plural = 'Заболевания'
-
-
-class Medical_drug(models.Model):
-    drug_id =models.IntegerField(primary_key=True)
-    drug_name = models.CharField(max_length=150)
-    sphere_id = models.ForeignKey('Sphere', on_delete=models.CASCADE)
-    # автоматом ставим время только при изменении объекта, а не при создании
-    time_create = models.DateTimeField(auto_now=True) 
-    time_form = models.DateTimeField(auto_now=True, auto_now_add=False) 
-    time_finish = models.DateTimeField(auto_now=True, auto_now_add=False)
-    price = models.IntegerField(default=0)
-    for_disease = models.ManyToManyField('Disease', through='DiseaseDrug')
-    sphere_id = models.OneToOneField(Sphere, on_delete=models.CASCADE)
-    STATUSES = [
-        ('e', 'entered'), # введён
-        ('o', 'in operation'), # в работе
-        ('f', 'finished'), # завершён
-        ('c', 'cancelled'), # отменён 
-        ('d', 'deleted') # удалён
-    ]
-    status = models.CharField(max_length=1, choices=STATUSES)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.drug_name
-    
-    
-
-    class Meta:
-        verbose_name_plural = 'Медицинские препараты'
-    
-
-
-
-class DiseaseDrug(models.Model):
-    disease_id = models.ForeignKey(Disease, on_delete=models.CASCADE)
-    drug_id = models.ForeignKey(Medical_drug, on_delete=models.CASCADE)
-
-
-    class Meta:
-        verbose_name_plural = 'Заболевание-препарат'
 
 
 class User(models.Model):
@@ -90,10 +48,47 @@ class User(models.Model):
         return self.user_name
 
     class Meta:
-        managed = False
+        managed = True
         verbose_name_plural = 'Пользователи'
 
 
-class User_test(models.Model):
-    user = models.IntegerField(primary_key=True)
+class Medical_drug(models.Model):
+    drug_id =models.IntegerField(primary_key=True)
+    drug_name = models.CharField(max_length=150)
+    sphere_id = models.ForeignKey(Sphere, on_delete=models.CASCADE)
+    # автоматом ставим время только при изменении объекта, а не при создании
+    time_create = models.DateTimeField(auto_now_add=True) 
+    time_form = models.DateTimeField(auto_now=True) 
+    time_finish = models.DateTimeField(auto_now=True)
+    price = models.IntegerField(default=0)
+    for_disease = models.ManyToManyField(Disease, through='DiseaseDrug')
+    STATUSES = [
+        ('e', 'entered'), # введён
+        ('o', 'in operation'), # в работе
+        ('f', 'finished'), # завершён
+        ('c', 'cancelled'), # отменён 
+        ('d', 'deleted') # удалён
+    ]
+    status = models.CharField(max_length=1, choices=STATUSES)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.drug_name
+    
+    class Meta:
+        managed = True
+        verbose_name_plural = 'Медицинские препараты'
+    
+
+
+class DiseaseDrug(models.Model):
+    disease_id = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    drug_id = models.ForeignKey(Medical_drug, on_delete=models.CASCADE)
+
+    class Meta:
+        managed = True
+        verbose_name_plural = 'Заболевание-препарат'
+
+
+
    
