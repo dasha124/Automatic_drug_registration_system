50fbe8f555ccf03893f62706eb1a72356ca439f8
dasha_kon <62230044+dasha124@users.noreply.github.com>
2024-01-15 11:20:10 +0300

    Update views.py

--- list_of_diseases/views.py
+++ list_of_diseases/views.py
@@ -17,53 +17,6 @@ from rest_framework.decorators import api_view
 from operator import itemgetter
 
 
-import base64
- 
-def DiseasesList(request):
-    query = request.GET.get('query', '')
-    
-    print(query)
-    search_diseases = []
-    for disease in Disease.objects.filter(status='a'):
-        if query.lower() in disease.disease_name.lower():
-            search_diseases.append(disease)
-
-    if len(search_diseases)>0:
-        return render(request, 'diseases.html', {'data': {
-        'diseases': search_diseases
-    }})
-    else:
-        return render(request, 'diseases.html', {'data': {
-        'diseases': Disease.objects.filter(status='a')
-    }})
-
-    
-
-def GetDisease(request, id):
-    s = Disease.objects.get(disease_id=id).simptoms.split(',')
-    return render(request, 'disease.html', {'data': { 
-        'disease': Disease.objects.get(disease_id=id),
-        'simptoms': s
-        }  
-    })
-
-
-
-def Delete(self, id):
-    with connection.cursor() as cursor:
-        cursor.execute("update list_of_diseases_disease set status='d' WHERE disease_id = %s", [id])
-    return redirect("/")
-
-
-
-# def GetIm(request, id):
-#     with connection.cursor() as cursor:
-#         cursor.execute("select encode(list_of_diseases_disease.image, 'escape') as image from list_of_diseases_disease")
-#         response = HttpResponse(a, content_type = 'image/jpeg')
-#     return response
-
-
-#  /------------------------------------------------------------------------------------------
 
 # список заболеваний (услуг)
 @api_view(['GET'])
